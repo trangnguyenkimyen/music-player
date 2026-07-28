cd2734dbad5fd30ac59cac78c3e22dbaa6762de2
Kim Yen <Trangnguyenkimyen@gmail.com>
2026-07-28 22:37:13 +0700

    BEO - LOVE me

--- script.js
+++ script.js
@@ -1,36 +1,94 @@
 // Dữ liệu lrc
 const rawLrc = `
-[00:00.000] I'm so in love, I'm so in love
-[00:02.472] I don't ever wanna stop this ride that we're on
-[00:05.992] I don't ever wanna say goodbye
-[00:08.633] Then all of those nights, they would just be all for nothing
-[00:13.849] Third of October
-[00:16.134] We were never sober
-[00:17.833] First few times that we hung out
-[00:19.588] But we fell in love somehow
-[00:21.477] First time that I met you
-[00:23.181] I didn't have a damn clue
-[00:25.112] That I love everything about you
-[00:26.876] Now I can't think of life without you
-[00:29.778] Eh
-[00:31.033] did you know that you're my whole heart?
-[00:33.471] Eh
-[00:34.618] did you know that I never stop?
-[00:37.119] No
-[00:38.188] Giving you everything I got 'cause I'm so
-[00:43.659] I'm so in love, I'm so in love
-[00:46.163] I don't ever wanna stop this ride that we're on
-[00:49.454] I don't ever wanna say goodbye
-[00:52.046] Then all of those nights,
-[00:54.057] they would just be all for nothing
-[00:56.931] Yeah, I'm so in love, I'm so in love
-[01:00.571] I don't ever wanna stop this ride that we're on
-[01:03.915] I don't ever wanna say goodbye
-[01:06.654] Then all of those nights,
-[01:08.699] they would just be all for nothing (yeah)
-[01:13.111] I'll never go back, I'll never go
-[01:16.891] Now that I know that, now that I know
-[01:20.541] I'll never leave you by your own
+[00:12.229] Hey girl, where you at?
+[00:13.493] (Where you at?)
+[00:15.176] Daeche eodiseo mwo haneunji gunggeumhae
+[00:18.023] Gibuni neomu bad, bad
+[00:20.269] (So bad)
+[00:21.520] Chimdaega heojeonhae jami an ojana
+[00:24.468] Jaemido eopseo TV show
+[00:26.506] Neo derigo noldeon pretty boy
+[00:28.102] Geu gomineul naege teoreonoteon
+[00:29.761] Neoui pyojeongi ajik seonmyeonghae
+[00:31.400] Sopae ben ni hyanggiga birithae
+[00:32.995] Nae momi jakku ppajyeo gipi, yeah
+[00:34.812] Miri arado nan iri
+[00:36.121] Mеongcheonghage haengdongеul haebeorindanikka
+[00:38.383] Oh, please
+[00:39.100] Love me, love me, love me
+[00:40.251] I know, I'm stupid
+[00:41.639] Eojjeol su eopseo neol bogo sipeungeol
+[00:44.039] Oh, please
+[00:45.403] Love me, love me, love me
+[00:46.591] I know, I'm stupid
+[00:48.014] Eojjeol su eopseo neol bogo sipeungeol
+[00:50.482] Now I say, I love you
+[00:52.053] So I love you
+[00:53.638] Now I say, I love you
+[00:55.198] Na apeudeorado molla
+[00:57.149] Now I say, I love you
+[00:58.542] So, I love you
+[00:59.962] Now I say, I love you
+[01:01.619] Na apeudeorado molla
+[01:03.959] Oh, please
+[01:05.006] Love me, love me, love me
+[01:06.019] I know, I'm stupid
+[01:07.419] Eojjeol su eopseo neol bogo sipeungeol
+[01:10.380] Oh, please
+[01:11.381] Love me, love me, love me
+[01:12.564] I know, I'm stupid
+[01:13.969] Eojjeol su eopseo neol bogo sipeungeol
+[01:16.268] Now I say, I love you
+[01:17.867] Try, try
+[01:18.817] Malhalkka nae burane buraneul
+[01:20.321] Kkeunkyeotdaneun Wi-Fi
+[01:21.745] Ijen an mideo Lie, lie
+[01:23.360] Maeil ppajyeoitdeon neoui banjigachi
+[01:25.001] Heojeonham saisai
+[01:26.581] Geu jariui gihoereul
+[01:27.944] Amuegena jugo tto, bye-bye
+[01:30.075] Neoneun neom ppalli deulleotda gagi ttaeme
+[01:32.501] Naneun taiming an maja maeil bappi
+[01:33.681] Hwareul naeya halji animyeon geunyang agicheoreom
+[01:35.348] Ureobeorigoseo ttereul sseoya halji
+[01:37.062] I don't know aye moreugesseo
+[01:38.499] Ireoni naega noryeokaedo
+[01:40.013] Sigani eopseuni eojjeogesseo
+[01:41.570] Hal mari hanabakke deo itgesseo
+[01:43.246] Oh, please
+[01:43.907] Love me, love me, love me
+[01:45.195] I know, I'm stupid
+[01:46.496] Eojjeol su eopseo neol bogo sipeungeol
+[01:48.889] Oh, please
+[01:50.253] Love me, love me, love me
+[01:51.489] I know, I'm stupid
+[01:52.812] Eojjeol su eopseo neol bogo sipeungeol
+[01:59.578] Now I say, I love you
+[02:01.399] So, I love you
+[02:03.030] Now I say, I love you
+[02:04.558] Na apeudeorado molla
+[02:06.698] Now I say, I love you
+[02:07.933] So, I love you
+[02:09.455] Now I say, I love you
+[02:11.052] Na apeudeorado molla
+[02:13.456] Oh, please
+[02:14.278] Love me, love me, love me
+[02:15.332] I know, I'm stupid
+[02:16.797] Eojjeol su eopseo neol bogo sipeungeol
+[02:19.664] Oh, please
+[02:20.627] Love me, love me, love me
+[02:21.876] I know, I'm stupid
+[02:23.196] Eojjeol su eopseo neol bogo sipeungeol
+[02:25.561] Now I say, I love you
+[02:27.218] Oh, please
+[02:27.850] Love me, love me, love me
+[02:28.762] Oh, please
+[02:30.440] Love me, love me, love me
+[02:31.817] Oh, please
+[02:33.593] Love me, love me, love me
+[02:34.854] I know, I'm stupid
+[02:36.257] Eojjeol su eopseo neol bogo sipeungeol
+[02:38.594] Now I say, I love you
 `;
 
 function parseLrc(lrcString) {
