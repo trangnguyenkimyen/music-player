f3a2d91bc597a78c3a40af719f32b6cc0d689125
Kim Yen <Trangnguyenkimyen@gmail.com>
2026-08-21 17:41:00 +0700

    gd- take me

--- script.js
+++ script.js
@@ -1,60 +1,59 @@
 // Dữ liệu lrc
 const rawLrc = `
-[00:04.82]Baby, are you down, down, down, down, down? (Oh, oh)
-[00:08.24]Down, down
-[00:12.16]Even if the sky is falling down
-[00:15.52]Down, down (ooh, ooh)
-[00:21.12]You oughta know (know, know)
-[00:24.43]Tonight is the night to let it go
-[00:28.41]Put on a show (show, show)
-[00:31.77]I wanna see how you lose control
-[00:35.33]So leave it behind
-[00:37.03]'Cause we have a night to get away (away, away)
-[00:42.63]So come on and fly with me
-[00:44.89]As we make our great escape (escape, escape)
-[00:49.95]So baby, don't worry, you are my only
-[00:53.99]You won't be lonely even if the sky is falling down
-[00:59.44]You'll be my only, no need to worry
-[01:02.99]Baby, are you down, down, down, down, down?
-[01:06.27](Down, down)
-[01:10.38]Baby, are you down, down, down, down, down?
-[01:13.68](Down, down)
-[01:17.50]Even if the sky is falling down
-[01:21.23]Just let it be
-[01:22.77]Come on and bring your body next to me
-[01:26.56]I'll take you away, hey
-[01:29.86]Turn this place into our private getaway
-[01:33.71]So leave it behind
-[01:35.24]'Cause we have a night to get away (away, away)
-[01:40.72]So come on and fly with me
-[01:43.27]As we make our great escape
-[01:46.21]So why don't we run away?
-[01:48.25]Baby, don't worry, you are my only (ooh)
-[01:51.98]You won't be lonely (no) even if the sky is falling down
-[01:57.53]You'll be my only (yeah), no need to worry (no)
-[02:01.29]Baby, are you down, down, down, down, down?
-[02:04.65](Down, down) whoo
-[02:08.47]Baby, are you down, down, down, down, down?
-[02:11.71](Down, down) mm, you down?
-[02:15.78]Even if the sky is falling down
-[02:18.48]Down like she 'posed to be, she gets down low for me
-[02:22.20]Down like her temperature, 'cause to me, she zero degrees
-[02:25.51]She cold, over-freeze, I got that girl from overseas
-[02:29.20]Now she's my Miss America, now can I be her soldier, please?
-[02:32.82]I'm fighting for this girl on a battlefield of love
-[02:36.20]Don't it look like baby Cupid sendin' her arrows from above?
-[02:39.86]Don't you ever leave the side of me, indefinitely, not probably
-[02:43.62]And honestly, I'm down like the economy, yeah
-[02:46.63]Baby, don't worry, you are my only
-[02:50.25]You won't be lonely (whoa) even if the sky is falling down
-[02:55.60]You'll be my only (no), no need to worry (no)
-[02:59.41]Baby, are you down, down, down, down, down?
-[03:02.74](Down, down) mm, down (down, down)
-[03:06.70]Baby, are you down, down, down, down, down?
-[03:09.34]Ooh, no (down, down), and the sky is fallin' down
-[03:13.72]Even if the sky is falling down
-[03:16.57]Ooh, no (ha-ha-ha)
-[03:19.51]And the sky is falling down
+[00:03.36]M-B-T-M-I-U
+[00:11.05]M-B-T-M-I-U
+[00:14.88]꿈에도 암만 낯설는지 몰라 | Even in my dreams, I don't know why you feel so unfamiliar
+[00:18.63]부담스럽지마는 설레잖아 | It feels a little overwhelming, but my heart still flutters
+[00:22.42]아무 말 없이, 숙맥 타 더 좋아 | Without a word, I like your awkward side even more
+[00:26.63]You're mine and I'm yours
+[00:30.08]너의 모든 게 너무 아름다워 | Everything about you is so beautiful
+[00:33.84]어떡해, 대체 불가한 여자면? | What do I do when there's no one else like you?
+[00:37.60]그대여, 이제 내 사랑을 받아 | My love, now accept my love
+[00:41.82]You're mine and I'm yours
+[00:46.24]별이 빛나는 이 밤 | On this starry night
+[00:48.00]가슴속 떨리는 마음 | My heart is trembling inside
+[00:50.09]너를 향해 모두 줄게, baby | I'll give you all of me, baby
+[00:53.88]좀 더 내게 다가와 | Come a little closer to me
+[00:55.80]내 두 손을 잡아봐 | Take both of my hands
+[00:57.66]나를 너에게 줄게 | I'll give myself to you
+[01:00.60]Take me, take me, take me, I'm yours
+[01:02.83]Take me, take me, I'm yours
+[01:04.88]Take me, take me, I'm yours
+[01:08.16]Take me, take me, take me, I'm yours
+[01:10.56]Take me, take me, I'm yours
+[01:12.34]Take me, take me, I'm yours
+[01:15.86]너의 눈을 보고 있으면 알아 | I can tell just by looking into your eyes
+[01:19.54]제발 아니라고 말하지 말아 | Please don't tell me it's not true
+[01:23.31]숨기려 해도, 태가 숨지 않아 | Even if you try to hide it, it still shows
+[01:27.46]Are you mine? 'Cause I'm yours
+[01:31.97]별이 빛나는 이 밤 | On this starry night
+[01:33.80]가슴속 떨리는 마음 | My heart is trembling inside
+[01:35.71]너를 향해 모두 줄게, baby | I'll give you all of me, baby
+[01:39.59]좀 더 내게 다가와 | Come a little closer to me
+[01:41.50]내 두 손을 잡아봐 | Take both of my hands
+[01:43.35]나를 너에게 줄게 | I'll give myself to you
+[01:46.22]Take me, take me, take me, I'm yours
+[01:48.53]Take me, take me, I'm yours
+[01:50.52]Take me, take me, I'm yours
+[01:53.87]Take me, take me, take me, I'm yours
+[01:56.29]Take me, take me, I'm yours
+[01:58.19]Take me, take me, I'm yours
+[02:01.59]Baby, baby, baby (ooh)
+[02:09.13]Baby, baby (ooh), baby (ooh)
+[02:17.93]빠르게 흘러만 가는 이 시간이 싫어 | I hate how quickly time keeps passing by
+[02:21.78]군침만 도는, mm | You're only making me want you more, mm
+[02:25.56]달리 난 너를 보낼 수는 없어 | I just can't let you go
+[02:28.35]Hurry up, baby, I'm yours
+[02:47.11]Take me, take me, take me, I'm yours
+[02:49.59]Take me, take me, I'm yours
+[02:51.42]Take me, take me, I'm yours
+[02:54.79]Take me, take me, take me, I'm yours
+[02:57.15]Take me, take me, I'm yours
+[02:59.12]Take me, take me, I'm yours
+[03:02.42]Baby, baby, baby (ooh)
+[03:10.02]Baby, baby (ooh), baby (ooh)
+[03:17.79]Baby, baby (ooh), baby (ooh)
+[03:25.38]Baby, baby (ooh), baby (ooh)
 `;
 
 function parseLrc(lrcString) {
